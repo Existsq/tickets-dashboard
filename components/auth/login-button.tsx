227ea7338c1a0830bc0144f8253ba16a5a0ba871
Exist <existqq@gmail.com>
2024-08-28 19:43:19 +0300

    Refactored + Presave

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,22 +1,10 @@
-// components/LoginButton.tsx
 "use client";
 import { Button } from "../ui/button";
 import { FaDiscord } from "react-icons/fa";
 
 const LoginButton = () => {
-
-  const handleLogin = () => {
-    const clientId = process.env.DISCORD_CLIENT_ID!;
-    const redirectUri = encodeURIComponent(process.env.DISCORD_REDIRECT_URI!);
-    const scope = encodeURIComponent("identify email guilds");
-    const responseType = "code";
-    const discordAuthUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
-
-    window.location.href = discordAuthUrl;
-  };
-
   return (
-    <Button variant="default" onClick={handleLogin} className="flex gap-2 w-full">
+    <Button variant="default" className="flex gap-2 w-full">
       <FaDiscord size={20} color="#000" />
       Continue with Discord
     </Button>
--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import { FC } from 'react';
-
-const LogoutButton: FC = () => {
-    const handleLogout = async () => {
-        await fetch('/api/auth/logout', { method: 'POST' });
-        window.location.href = '/sign-in';
-    };
-
-    return (
-        <button onClick={handleLogout}>
-            Log out
-        </button>
-    );
+const LogoutButton = () => {
+  return <button>Log out</button>;
 };
 
 export default LogoutButton;
